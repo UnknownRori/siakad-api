cebb51d6a8c82df9702b936bfc41ba091af50262
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-02-18 07:10:07 +0700

    📦 Scaffold project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Siakad an Online Campus Management App
+
+*It will soon be added, since it's still and idea
+
+# Design
+
+*It will soon be added, the draft will be available on figma
+
+# 🛠️ Development & Running
+
+```bash
+# git clone this repository
+> git clone https://github.com/UnknownRori/siakad
+```
+
+# 🌟 Contribution
+
+Feel free to contribute and improve the codebase, send pull request or create an issues if something is wrong or need some additional feature we will review it.
---
+++ web-vue/tailwind.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+    content: [
+        "./src/**/*.{ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            screens: {
+                'sm': '240px',
+                'md': '640px',
+                'lg': '800px',
+                'xl': '1024px',
+                '2xl': '1280px',
+            },
+            keyframes: {
+                fadeInLeft: {
+                    '0%': {
+                        opacity: '0%',
+                        transform: 'translateX(-100vw)'
+                    },
+                    '80%': {
+                        transform: 'translateX(2vw)'
+                    },
+                    '100%': {
+                        opacity: '100%',
+                        transform: 'translateX(0px)'
+                    }
+                },
+                fadeOutLeft: {
+                    '0%': {
+                        transform: 'translateX(0vw)'
+                    },
+                    '15%': {
+                        opacity: '100%',
+                        transform: 'translateX(2vw)'
+                    },
+                    '100%': {
+                        opacity: '0%',
+                        transform: 'translateX(-100vw)',
+                    }
+                }
+            },
+            animation: {
+                fadeInLeft: 'fadeInLeft 1s ease-in-out forwards',
+                fadeOutLeft: 'fadeOutLeft 1s ease-in-out forwards',
+            }
+        },
+    },
+    plugins: [],
+}

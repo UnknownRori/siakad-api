fbe76375ab8fea1bee833070cb48d5be162caf32
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-02-25 04:03:44 +0700

    📦 Added I18n

---
+++ web-vue/src/i18n/id.ts
@@ -0,0 +1,3 @@
+export default {
+    hello: "halo, dunia!",
+};
--- web-vue/src/main.ts
+++ web-vue/src/main.ts
@@ -1,14 +1,29 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import { createI18n } from "vue-i18n";
 
-import './assets/main.css'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import "./assets/main.css";
 
-app.use(createPinia())
-app.use(router)
+import enUs from "./i18n/en-us";
+import id from "./i18n/id";
 
-app.mount('#app')
+const i18n = createI18n({
+    locale: "id",
+    fallbackLocale: "en-us",
+    messages: {
+        id: id,
+        "en-us": enUs,
+    },
+});
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(i18n);
+app.use(router);
+
+app.mount("#app");

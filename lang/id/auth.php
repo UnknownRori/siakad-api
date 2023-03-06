aad566bb98f7854eddc9b3e1663ec8a1e60408cd
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-03-06 09:05:52 +0700

    📦 Added basic indonesian translation

---
+++ lang/id/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Data yang dimasukan tidak sesuai dengan data kami.',
+    'password' => 'Password yang dimasukan salah.',
+    'throttle' => 'Terlalu banyak percobaan login, coba lagi di lain waktu.',
+];
---
+++ lang/id/passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'reset' => 'Passwordmu telah di reset!',
+    'sent' => 'Kami sudah mengirimkan email link untuk me-reset password mu!',
+    'throttled' => 'Mohon bersabar sebelum mengulang.',
+    'token' => 'Token reset password sudah hangus',
+    'user' => 'Kami tidak dapat mencari user yang memiliki alamat email tersebut.',
+
+];

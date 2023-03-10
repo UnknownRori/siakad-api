68763efea99d0bf939d4eb39833b0783792252fe
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-03-11 04:06:32 +0700

    🛠️ Disable auto generate email

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,8 +19,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            // 'email' => fake()->unique()->safeEmail(),
+            // 'email_verified_at' => now(),
             'password' => 'password',
             'remember_token' => Str::random(10),
         ];

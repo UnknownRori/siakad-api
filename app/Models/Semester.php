2f10f711e988e07f7829d7c9190db668f5133117
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-03-11 04:07:06 +0700

    📦 Added relationship for Course and Semester

--- app/Models/Semester.php
+++ app/Models/Semester.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Semester extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    public function courses(): HasMany
+    {
+        return $this->hasMany(Course::class);
+    }
 }
